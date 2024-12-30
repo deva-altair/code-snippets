0a709c9c87c5150239f14048a72256605070eb1b
Wojka <solana369@outlook.com>
2024-12-30 23:11:41 +0100

    Secrets clean-up, deploy file added.

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCsTkjWEiPi6OvkqgPn5Bm0Z_8TOSFBtnM",
-    authDomain: "code-snippets-69637.firebaseapp.com",
-    projectId: "code-snippets-69637",
-    storageBucket: "code-snippets-69637.firebasestorage.app",
-    messagingSenderId: "777375833541",
-    appId: "1:777375833541:web:33056b7f3660def2c93fac"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
 
   const app = initializeApp(firebaseConfig);
